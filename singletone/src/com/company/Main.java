c4c7ef95f13e52b08ccc9a86b7423d476f2edf64
Jora Gevorgyan <jora@ispras.ru>
2021-10-13 13:40:25 +0400

    a simple singletone class example

---
+++ singletone/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import single.Single;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+
+        Single single = Single.getSingle();
+
+        single.showMsg(message);
+    }
+}
---
+++ singletone/src/single/Single.java
@@ -0,0 +1,21 @@
+package single;
+
+public class Single {
+    private static Single object;
+    private Single() {}
+
+    public static Single getSingle() {
+        if (object == null) {
+            synchronized (Single.class) {
+                object = new Single();
+            }
+            return object;
+        }
+        return object;
+    }
+
+    public void showMsg(String msg) {
+        System.out.println("Got the following message from user:");
+        System.out.print(msg);
+    }
+}
