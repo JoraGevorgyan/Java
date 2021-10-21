46d93cdb21575f5282800f1d5b213bf39568ae0c
jora <jora@ispras.ru>
2021-10-21 16:01:37 +0400

    use all classes in main

--- Interfaces/src/com/company/Main.java
+++ Interfaces/src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import Interfaces.Drawable;
+import Shapes.Circle;
+import Shapes.Shape;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Drawable> shapes = new ArrayList<>();
+        int index = 0;
+        shapes.add(index++, new Shape("Rectangle"));
+        shapes.add(index++, new Circle(index * index));
+        shapes.add(index++, new Shape("Square"));
+
+        for (int i = 0; i < index; ++i) {
+            shapes.get(i).draw();
+        }
     }
 }
